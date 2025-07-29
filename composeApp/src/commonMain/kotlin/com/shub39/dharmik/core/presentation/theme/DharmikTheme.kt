13c433ecb1e6186dcbae06e0e618c6029e5073f9
shub39 <cptnshubham39@gmail.com>
2025-07-29 13:11:30 +0530

    :wrench: Updated deps
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- composeApp/src/commonMain/kotlin/com/shub39/dharmik/core/presentation/theme/DharmikTheme.kt
+++ composeApp/src/commonMain/kotlin/com/shub39/dharmik/core/presentation/theme/DharmikTheme.kt
@@ -1,22 +1,24 @@
 package com.shub39.dharmik.core.presentation.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.runtime.*
-import com.materialkolor.DynamicMaterialTheme
+import com.materialkolor.DynamicMaterialExpressiveTheme
 import com.shub39.dharmik.core.domain.AppTheme
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun DharmikTheme(
     theme: Theme = Theme(),
     content: @Composable () -> Unit
 ) {
-    DynamicMaterialTheme(
-        useDarkTheme = when(theme.appTheme) {
+    DynamicMaterialExpressiveTheme(
+        isDark = when(theme.appTheme) {
             AppTheme.LIGHT -> false
             AppTheme.DARK -> true
             else -> isSystemInDarkTheme()
         },
-        withAmoled = theme.withAmoled,
+        isAmoled = theme.withAmoled,
         seedColor = theme.seedColor,
         style = theme.paletteStyle,
         typography = provideTypography(1f),
